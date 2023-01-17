28cad45d7b1a80ed9be4b8cdf5062efe9b4c9319
YJS_92 <kalwlstjd@gmail.com>
2023-01-18 05:40:18 +0900

    개발용이라 http 접속 소스 추가

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const serverUrl = "http://ec2-52-78-154-77.ap-northeast-2.compute.amazonaws.com:9000";
 // const serverUrl = "http://localhost:9000";
+const currentUrl = window.location.href;
 
 class Main extends Component {
     state = {
@@ -10,69 +11,80 @@ class Main extends Component {
         location:[],
         data:[]
     };
-
-    
     
     // 1. geolocation api 내위치 사용
     // 2. 좌표를 구한뒤 공공데이터 api 로 측정소 위치 구하기
     // 3. 측정소 위치의 미세먼지 정보 구하기            
     getLocation = async () => {
+        let latitude = "";
+        let longitude = "";
+        let textContent = "";
+
         if('geolocation' in navigator) {
             /* 위치정보 사용 가능 */
     
             navigator.geolocation.getCurrentPosition(pos => {
-                let latitude = pos.coords.latitude;
-                let longitude = pos.coords.longitude;
-                let textContent = 'Your location data is ' + latitude + ', ' + longitude;
+                latitude = pos.coords.latitude;
+                longitude = pos.coords.longitude;
+                textContent = 'Your location data is ' + latitude + ', ' + longitude;
                 console.log(textContent);
-    
-                // 1. 통계청 auth key 생성
-                let url1 = serverUrl + "/datago/sgisAuth";
-                axios.get(url1).then((response1) => {
-                    // console.log(JSON.parse(response.data).response.body);
-                    console.log(response1.data.result);
-                    let param1 = response1.data.result.accessToken;
-                     
-                    // 2. 통계청 TM 좌표로 변환 생성
-                    let url2 = serverUrl + "/datago/sgisTM?accessToken=" + encodeURIComponent(param1)
-                                                            + "&latitude="+ encodeURIComponent(latitude)
-                                                            + "&longitude=" + encodeURIComponent(longitude);
-                    axios.get(url2).then((response2) => {
-                        console.log(response2.data.result);
-                        let posX = response2.data.result.posX;
-                        let posY = response2.data.result.posY;
-                        
-                        // 3. 미세먼지 근접측정소 검색
-                        let url3 = serverUrl + "/datago/Mmethod2?posX="+ encodeURIComponent(posX)
-                                                                    + "&posY=" + encodeURIComponent(posY);
-    
-                        axios.get(url3).then((response3) => {
-                            console.log(JSON.parse(response3.data).response.body);
-    
-                            // index 0을 가장 가까운 측정소로 한다
-                            let locations = JSON.parse(response3.data).response.body.items;
-                            this.setState({ location: locations, isLoading: true });
 
-                            // 4. 미세먼지 정보 GET
-                            let url4 = serverUrl + "/datago/Amethod1"
-                                                    + "?stationName="+ encodeURIComponent(locations[0].stationName);
-    
-                            axios.get(url4).then((response4) => {
-                                console.log(JSON.parse(response4.data).response.body);
-                                
-                                let datas = JSON.parse(response4.data).response.body.items;
-                                this.setState({ data: datas, isLoading: false });
-                            });
-                        });
-                    });
-                });
-                
             }, err => {
                 console.log(err);
             });
         } else {
             /* 위치정보 사용 불가능 */
             console.log('위치정보 사용 불가능');
+        }
+
+        if(currentUrl.indexOf('https') > 0){
+            // http로 이동
+            window.location.href='http://'+window.location.host+'/?latitude='+latitude+'&longitude='+longitude;
+        } else {
+            const params = new URLSearchParams(window.location.search);
+            latitude = params.get(latitude);
+            longitude = params.get(longitude);
+
+            // 1. 통계청 auth key 생성
+            let url1 = serverUrl + "/datago/sgisAuth";
+            axios.get(url1).then((response1) => {
+                // console.log(JSON.parse(response.data).response.body);
+                console.log(response1.data.result);
+                let param1 = response1.data.result.accessToken;
+                
+                // 2. 통계청 TM 좌표로 변환 생성
+                let url2 = serverUrl + "/datago/sgisTM?accessToken=" + encodeURIComponent(param1)
+                                                        + "&latitude="+ encodeURIComponent(latitude)
+                                                        + "&longitude=" + encodeURIComponent(longitude);
+                axios.get(url2).then((response2) => {
+                    console.log(response2.data.result);
+                    let posX = response2.data.result.posX;
+                    let posY = response2.data.result.posY;
+                    
+                    // 3. 미세먼지 근접측정소 검색
+                    let url3 = serverUrl + "/datago/Mmethod2?posX="+ encodeURIComponent(posX)
+                                                                + "&posY=" + encodeURIComponent(posY);
+
+                    axios.get(url3).then((response3) => {
+                        console.log(JSON.parse(response3.data).response.body);
+
+                        // index 0을 가장 가까운 측정소로 한다
+                        let locations = JSON.parse(response3.data).response.body.items;
+                        this.setState({ location: locations, isLoading: true });
+
+                        // 4. 미세먼지 정보 GET
+                        let url4 = serverUrl + "/datago/Amethod1"
+                                                + "?stationName="+ encodeURIComponent(locations[0].stationName);
+
+                        axios.get(url4).then((response4) => {
+                            console.log(JSON.parse(response4.data).response.body);
+                            
+                            let datas = JSON.parse(response4.data).response.body.items;
+                            this.setState({ data: datas, isLoading: false });
+                        });
+                    });
+                });
+            });
         }
     }
 
